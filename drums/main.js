9914e051cce08fbcb9409d7cdc8ad3349231d575
Froks15 <frokstv@gmail.com>
2018-01-11 18:00:42 +0400

    commit

--- drums/main.js
+++
@@ -1,9 +0,0 @@
-window.addEventListener('keydown', playSound);
-
-function playSound(e) {
-    var audio = document.querySelector(`audio[data-id="${e.keyCode}"]`);
-    if(!audio) { return };
-
-    audio.currentTime = 0;
-    audio.play();
-}
